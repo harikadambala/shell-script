76bf20774c1591f79c11916e249f72786c42b016
harikadambala <harikadambala131@gmail.com>
2024-02-19 17:27:14 +0530

    script

--- 04-variables.sh
+++ 04-variables.sh
@@ -4,7 +4,7 @@
 PERSON=$1
 PERSON=$2
 
-echo "$PERSON1:hello $PERSON2, good morning"
-echo "$PERSON2:hi $PERSON1,verygood morning"
-echo "$PERSON1:how are you $PERSON2"
-echo "$PERSON2:i am good $PERSON1, how are you"
+echo "$1:hello $2, good morning"
+echo "$2:hi $PERSON1,verygood morning"
+echo "$1:how are you $2"
+echo "$2:i am good $1, how are you"
