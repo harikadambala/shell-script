31a6f88cac2e21ffa1093c64e441de86fe01ea5f
harikadambala <harikadambala131@gmail.com>
2024-02-19 13:13:34 +0530

    shell

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 
-echo "hari: hello tiru, good morning"
-echo "tiru: hi hari, very good morning"
-echo "hari: how are you tiru?"
-echo "tiru: i am good hari, how are you"
+
+echo "$hari: hello $tiru, good morning"
+echo "$tiru: hi $hari, very good morning"
+echo "$hari: how are you $tiru?"
+echo "$tiru: i am good $hari, how are you"
