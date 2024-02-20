8b70051a2b060ccecbd3c4244f08cd753c3eeafc
harikadambala <harikadambala131@gmail.com>
2024-02-20 23:26:23 +0530

    commiting scripts

--- 10-functions.sh
+++ 10-functions.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 ID=$(id -u)
-validate (){
+
     if[$? -ne 0]
     then
        echo "error:: installing mysql is failed"
     else
        echo "installing mysql is success"
     fi   
-}
+
