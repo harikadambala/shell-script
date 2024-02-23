96983293aec746bb2fec45e37e5ca2428df646e6
harikadambala <harikadambala131@gmail.com>
2024-02-23 17:52:25 +0530

    script

--- 13-install-package.sh
+++ 13-install-package.sh
@@ -13,13 +13,13 @@ LOGFILE="/tmp/$0-$TIMESTAMP.log"
 echo "script started executing at $TIMESTAMP" $>> $LOGFILE
 
 
-VALIDATE(){
+VALIDATE()
     if [$1 -ne 0]
     then
         echo -e "$2 ...$R FAILED $N"
     else
         echo -e "$2 ...$G success $N"
-}
+
 
 if [ $ID -ne 0 ]
 then
