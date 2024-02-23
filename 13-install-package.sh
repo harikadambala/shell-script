d74f9156f795b6798d610c2d126d65f44761105f
harikadambala <harikadambala131@gmail.com>
2024-02-23 17:44:54 +0530

    script

--- 13-install-package.sh
+++ 13-install-package.sh
@@ -3,9 +3,23 @@
 ID=$(id -u)
 R="\e[31m"
 G="\e[32m"
+Y="\e[33m"
 N="\e[0m"
 
 
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script started executing at $TIMESTAMP" $>> $LOGFILE
+
+
+VALIDATE(){
+    if [$1 -ne 0]
+    then
+        echo -e "$2 ...$R FAILED $N"
+    else
+        echo -e "$2 ...$G success $N"
+}
 
 if [ $ID -ne 0 ]
 then
@@ -15,6 +29,22 @@ else
     echo "You are root user"
 fi # f1 means reverse of if, indicating condition end
       
-echo "All arguments passed: $@"
+#echo "All arguments passed: $@"
+# git mysql postfix net-tools
+# package=git for first time
+
+for package in $@
+do
+    yum list installed $package $>> $LOGFILE #check installed or not
+
+    if[ $? -ne 0 ] #if not installed
+    then
+        yum install $package -y $>> $LOGFILE # install the package  
+        VALIDATE $? "installation of $package" # validate
+    else
+       echo"$package is already installed ... $Y SKIPPING $N"
+     
+
+done
 
 
